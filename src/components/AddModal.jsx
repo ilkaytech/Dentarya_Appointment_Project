0ff57b83376c2663bddeb6009e9dc01d384de984
ilkay <06ilkayozturk@gmail.com>
2023-09-12 19:47:44 +0300

    edit

--- src/components/AddModal.jsx
+++ src/components/AddModal.jsx
@@ -11,34 +11,25 @@ function AddModal({ show, handleClose }) {
         </Modal.Header>
         <Modal.Body>
           <Form>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
+            <Form.Group className="mb-3" controlId="name">
+              <Form.Label>Patient Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter Name" />
             </Form.Group>
 
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" />
+            <Form.Group className="mb-3" controlId="date">
+              <Form.Label>Day&Time</Form.Label>
+              <Form.Control type="datetime-local" placeholder="Date" />
             </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-              <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
+            <div className="text-end ">
+              <Button variant="primary" type="submit" className="me-2">
+                Save
+              </Button>
+              <Button variant="danger" onClick={handleClose}>
+                Close
+              </Button>
+            </div>
           </Form>
         </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-          <Button variant="primary" onClick={handleClose}>
-            Save Changes
-          </Button>
-        </Modal.Footer>
       </Modal>
     </>
   );
