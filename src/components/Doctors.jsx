a8879dbdbdffece1f035c0fa67cdb670be43418b
ilkay <06ilkayozturk@gmail.com>
2023-09-12 17:13:05 +0300

    responsive

--- src/components/Doctors.jsx
+++ src/components/Doctors.jsx
@@ -1,4 +1,6 @@
-import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container";
+import { doctorData } from "../helper/data";
+import { Row, Col } from "react-bootstrap";
 
 const Doctors = () => {
   return (
@@ -6,8 +8,18 @@ const Doctors = () => {
       <h3 className="display-6 mb-3" style={{ color: "rgb(166, 18, 189)" }}>
         Our Doctors
       </h3>
-    </Container>
-  )
-}
 
-export default Doctors
+      <Row>
+        {doctorData.map(({ id, img, dep, name }) => (
+          <Col xs={6} sm={4} md={3}>
+            <img src={img} alt={name} className="img-thumbnail doctor-img" />
+            <h5>{name} </h5>
+            <h6>{dep} </h6>
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
+
+export default Doctors;
