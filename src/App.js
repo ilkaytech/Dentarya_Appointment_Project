611aab607bd2a16daf62cb55395858a960c48a6f
ilkay <06ilkayozturk@gmail.com>
2023-09-12 13:02:54 +0300

    edit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import "./App.css"
-import Home from "./pages/Home"
+import "./App.css";
+import Home from "./pages/Home";
+// import { AiOutlineDelete } from "react-icons/ai";
 
 const App = () => {
-  return <Home />
-}
+  return (
+    <div>
+      <Home />
+      {/* <AiOutlineDelete className="text-danger fs-3" /> */}
+    </div>
+  );
+};
 
-export default App
+export default App;
