4c8a95b345a6c2532c2b6b16a30d1a5ee2b46d50
ilkay <06ilkayozturk@gmail.com>
2023-09-13 23:29:44 +0300

    appointmentData

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import AppointmentList from "../components/AppointmentList";
 import Doctors from "../components/Doctors";
+import { appointmentData } from "../helper/data";
+import { useState } from "react";
 
 const Home = () => {
+  const [appointments, setAppointments] = useState(appointmentData);
   return (
     <main className="text-center mt-2">
       <h1 className="display-5 text-danger">DENTARYA</h1>
