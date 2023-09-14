fde2ac57a084182c5a0d151ab97bff30460ba53c
ilkay <06ilkayozturk@gmail.com>
2023-09-14 22:07:38 +0300

    add_appointments

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -1,5 +1,6 @@
 import Container from "react-bootstrap/Container";
 import { Row, Col } from "react-bootstrap";
+import { RiCloseCircleLine } from "react-icons/ri";
 
 const AppointmentList = ({ apps }) => {
   console.log(apps);
@@ -8,15 +9,17 @@ const AppointmentList = ({ apps }) => {
       <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
         Appointment List
       </h3>
-      {apps.map((id, patient, consulted, doctor, day) => (
-        <div key={id}>
-          <Row className="">
+      {apps.map(({ id, patient, consulted, doctor, day }) => (
+        <div key={id} className="appointments">
+          <Row>
             <Col>
               <h4>{patient}</h4>
               <h5>{doctor}</h5>
             </Col>
             <Col>{day}</Col>
-            <Col>Icon</Col>
+            <Col>
+              <RiCloseCircleLine className="text-primary fs-5" />
+            </Col>
           </Row>
         </div>
       ))}
