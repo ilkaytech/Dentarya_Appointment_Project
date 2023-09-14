e10beb187d4d16597683ed909f7dad252328a9cc
ilkay <06ilkayozturk@gmail.com>
2023-09-14 21:23:55 +0300

    appList

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -1,13 +1,25 @@
-import Container from "react-bootstrap/Container"
+import Container from "react-bootstrap/Container";
+import { Row, Col } from "react-bootstrap";
 
-const AppointmentList = () => {
+const AppointmentList = ({ apps }) => {
+  console.log(apps);
   return (
     <Container className="p-2">
       <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
         Appointment List
       </h3>
+      <div>
+        <Row>
+          <Col>
+            <h4>İlkay Öztürk</h4>
+            <h5>Doctor Name</h5>
+          </Col>
+          <Col>Date</Col>
+          <Col>Icon</Col>
+        </Row>
+      </div>
     </Container>
-  )
-}
+  );
+};
 
-export default AppointmentList
+export default AppointmentList;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ const Home = () => {
     <main className="text-center mt-2">
       <h1 className="display-5 text-danger">DENTARYA</h1>
       <Doctors />
-      <AppointmentList />
+      <AppointmentList apps={appointments} />
     </main>
   );
 };
