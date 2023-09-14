397db657668ff170ef9be659fc5dd258c3bf1e55
ilkay <06ilkayozturk@gmail.com>
2023-09-14 21:27:52 +0300

    appList

--- src/components/AppointmentList.jsx
+++ src/components/AppointmentList.jsx
@@ -8,16 +8,18 @@ const AppointmentList = ({ apps }) => {
       <h3 className="display-6 mb-2" style={{ color: "rgb(166, 18, 189)" }}>
         Appointment List
       </h3>
-      <div>
-        <Row>
-          <Col>
-            <h4>İlkay Öztürk</h4>
-            <h5>Doctor Name</h5>
-          </Col>
-          <Col>Date</Col>
-          <Col>Icon</Col>
-        </Row>
-      </div>
+      {apps.map((id, patient, consulted, doctor, day) => (
+        <div key={id}>
+          <Row className="">
+            <Col>
+              <h4>{patient}</h4>
+              <h5>{doctor}</h5>
+            </Col>
+            <Col>{day}</Col>
+            <Col>Icon</Col>
+          </Row>
+        </div>
+      ))}
     </Container>
   );
 };
